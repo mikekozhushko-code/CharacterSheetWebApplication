a4206ce27f32ffb0b568c6002aee508e9bd45130
НАЗАР <urgisweb@gmail.com>
2026-01-20 22:44:59 +0200

    Setup accounts app with custom user model, profile, JWT auth, and React registration integration

--- morning-star/Frontend/src/pages/RegistrationPage.jsx
+++ morning-star/Frontend/src/pages/RegistrationPage.jsx
@@ -1,61 +1,86 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
-import '../styles/Login_style.css'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Login_style.css';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import api from "../Api.jsx";
+
 const RegistrationPage = () => {
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+    });
+    const navigate = useNavigate();
+
+    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await api.post("/register/", form);
+            navigate("/");
+        } catch (error) {
+            alert(`Error: ${error.message}`);
+        }
+    }
+
     return (
         <>
             <Header isAuthPage={true} btnText="Sign In" btnLink="/" />
-            
+
             <main>
                 <div className="login">
-                    <img 
-                            className="emblem"  
-                            src="/assets/images/Pass.svg" 
-                            alt="Pass" 
-                            width="379"         
-                            height="569" 
-                            style={{transform:' translateY(-97.1px) scale(0.95)  '}}       
-                        />
-                    
+                    <img
+                        className="emblem"
+                        src="/assets/images/Pass.svg"
+                        alt="Pass"
+                        width="379"
+                        height="569"
+                        style={{ transform: ' translateY(-97.1px) scale(0.95)  ' }}
+                    />
+
                     <div className="login-form">
-                         <div className="enter">
-                            <label className="login-form-text" htmlFor="email"><b>Email</b></label>
-                            <input className="input-login" type="email" name="email" required />
-                        </div>
-                        <div className="enter">
-                            <label className="login-form-text" htmlFor="uname"><b>Login</b></label>
-                            <input className="input-login" type="text" name="uname" required />
-                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="enter">
+                                <label className="login-form-text" htmlFor="email"><b>Email</b></label>
+                                <input className="input-login" type="email" name="email" onChange={handleChange} required />
+                            </div>
+                            <div className="enter">
+                                <label className="login-form-text" htmlFor="username"><b>Login</b></label>
+                                <input className="input-login" type="text" name="username" onChange={handleChange} required />
+                            </div>
 
-                        <div className="enter">
-                            <label className="login-form-text" htmlFor="pass"><b>Password</b></label>
-                            <input className="input-login" type="password" name="pass" required />
-                        </div>
-                         <div className="enter">
-                            <label className="login-form-text" htmlFor="pass-repeat"><b>Repeat Password</b></label>
-                            <input className="input-login" type="password" name="pass-repeat" required />
-                        </div>
-                        
-                        <button className="button-login" type="button">Sign up</button>
-                        <button className="button-login" type="button">
-                            Sign up by Google 
-                            <img className="google-icon" src="/assets/icons/google.svg" alt="Google" style={{transform:'translateY(4px)',width:"30px",height:"30px"}}/>
-                        </button>
+                            <div className="enter">
+                                <label className="login-form-text" htmlFor="password1"><b>Password</b></label>
+                                <input className="input-login" type="password" name="password1" onChange={handleChange} required />
+                            </div>
+                            <div className="enter">
+                                <label className="login-form-text" htmlFor="password2"><b>Repeat Password</b></label>
+                                <input className="input-login" type="password" name="password2" onChange={handleChange} required />
+                            </div>
 
-                        <div className="register-redirect">
-                            <span style={{ color: 'white'}}>Already have an account?</span>
-                            <Link to="/" className="register-link">
-                                Sign In
-                            </Link>
-                        </div>
+                            <button className="button-login" type="submit">Sign up</button>
+                            <button className="button-login" type="button">
+                                Sign up by Google
+                                <img className="google-icon" src="/assets/icons/google.svg" alt="Google" style={{ transform: 'translateY(4px)', width: "30px", height: "30px" }} />
+                            </button>
+
+                            <div className="register-redirect">
+                                <span style={{ color: 'white' }}>Already have an account?</span>
+                                <Link to="/" className="register-link">
+                                    Sign In
+                                </Link>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </main>
-            <Footer/>
+            <Footer />
         </>
     );
 };
