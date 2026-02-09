ff88911564e5f38acf0194a655cf63a6fb3785f7
NazarShubeliak <nazarshubeliak@ukr.net>
2026-02-09 18:23:02 +0200

    Add user model to admin panel

--- morning-star/Backend/characters/admin.py
+++ morning-star/Backend/characters/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import CustomUser   # твій кастомний юзер
+from characters.models import Character  # персонажі
 
-# Register your models here.
+class CharacterInline(admin.TabularInline):
+    model = Character
+    extra = 0
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [CharacterInline]
+
+    def character_count(self, obj):
+        return obj.character.count()
+    character_count.short_description = "Character"
+
+    list_display = UserAdmin.list_display + ("character_count",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
