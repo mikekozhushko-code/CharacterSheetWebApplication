edb01df7a8d14ba867f6cb9ebb1538e34206cb15
NazarShubeliak <nazarshubeliak@ukr.net>
2026-02-27 23:14:36 +0200

    fix: fixed attack state update and backend sync

--- morning-star/Backend/characters/models.py
+++ morning-star/Backend/characters/models.py
@@ -57,6 +57,8 @@ class Character(models.Model):
     # Coins
     wallet = models.JSONField(default=default_wallet)
 
+    # Inventar
+    attacks = models.JSONField(default=list, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
